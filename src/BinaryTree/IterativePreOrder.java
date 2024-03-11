c4dd4b622adda6ca1376f7af008de7b55f9ecc43
Tamil Selvi <tamilselvi072001@gmail.com>
2024-03-11 09:17:09 +0530

    Iterative preorder in binarytree

--- src/BinaryTree/IterativePreOrder.java
+++ src/BinaryTree/IterativePreOrder.java
@@ -2,10 +2,11 @@ package BinaryTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
-public class InOrder {
+public class IterativePreOrder {
     public static void main(String[] args) {
-        Node root = new Node(1);
+         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
@@ -16,18 +17,22 @@ public class InOrder {
         root.right.right.left = new Node(9);
         root.right.right.right = new Node(10);
         List<Integer> ans=new ArrayList<>();
-        ans=inorderTraversal(root);
+        ans=preOrderTraversal(root);
         for(int i=0;i<ans.size();i++){
             System.out.print(ans.get(i)+" ");
         }
     }
-    public static List<Integer> inorderTraversal(Node root) {
-        List<Integer> inOrderArr = new ArrayList<>();   
-        if (root == null) return inOrderArr;     
-        inOrderArr.addAll(inorderTraversal(root.left));  
-        inOrderArr.add(root.data);
-        inOrderArr.addAll(inorderTraversal(root.right)); 
-        return inOrderArr; 
+    public static List<Integer> preOrderTraversal(Node root){
+        Stack<Node> st=new Stack<>();
+        List<Integer> ans=new ArrayList<>();
+        st.add(root);
+        while(!st.isEmpty()){
+            Node currNode=st.pop();
+            ans.add(currNode.data);
+            if(currNode.right!=null) st.add(currNode.right);
+            if(currNode.left!=null) st.add(currNode.left);
+        }
+        return ans;
     }
 }
 class Node{
